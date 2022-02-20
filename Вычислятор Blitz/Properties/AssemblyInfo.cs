e393797e0827fec7b48e0a033d8ec799328afc6b
Sasha Garan <61148267+SashaGaran@users.noreply.github.com>
2022-02-20 22:53:02 +0200

    Delete Вычислятор Blitz directory

--- Вычислятор Blitz/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;using System.Reflection;
-[assembly: AssemblyTitle ("Вычислятор Blitz")]
-[assembly: AssemblyDescription ("Вычислятор Blitz")]
-[assembly: AssemblyCompany ("Компания хороших друзей")]
-[assembly: AssemblyProduct ("Вычислятор Blitz")]
-[assembly: AssemblyCopyright ("Sasha Garan 2021")]
-[assembly: AssemblyTrademark ("Какая-то торговая марка")]
-[assembly: AssemblyVersion ("3.2")]
-[assembly: AssemblyDefaultAlias ("Какой-то псевдоним")]
